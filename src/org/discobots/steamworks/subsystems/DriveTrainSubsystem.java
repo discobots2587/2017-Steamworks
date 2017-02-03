d2d763b6dad502fec9a74bab8ab28a7f3ea452a6
William Bittner <wbittnet@gmail.com>
2017-02-03 16:04:53 -0600

    Added unramped tank drive and unramped arcadedrive

--- src/org/discobots/steamworks/subsystems/DriveTrainSubsystem.java
+++ src/org/discobots/steamworks/subsystems/DriveTrainSubsystem.java
@@ -1,26 +1,44 @@
 package org.discobots.steamworks.subsystems;
 
+import org.discobots.steamworks.HW;
+
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class DriveTrainSubsystem extends Subsystem {
-private double speedScale;
+private double kSpeedScaling;
+private RobotDrive robotDrive;
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-
+	public DriveTrainSubsystem(){
+		robotDrive=new RobotDrive(HW.motorFrontLeft,HW.motorBackLeft,
+								  HW.motorFrontRight,HW.motorBackRight);
+	}
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
 
 	public void setSpeedScaling(double speedScale) {
-		this.speedScale=speedScale;
+		this.kSpeedScaling=speedScale;
 	}
 	public double getSpeedScaling(){
-		return speedScale;
+		return kSpeedScaling;
 }
+	public void arcadeDrive(double y, double x) {
+
+		robotDrive.arcadeDrive(x * kSpeedScaling, -y * kSpeedScaling);
+		// robotdrive is dumb arcadeDrive so params are switched
+	}
+	public void tankDriveUnramped(double leftStick, double rightStick) {
+
+		robotDrive.tankDrive(leftStick * kSpeedScaling, -rightStick
+				* kSpeedScaling);
+	}
+	
 
 }
 
