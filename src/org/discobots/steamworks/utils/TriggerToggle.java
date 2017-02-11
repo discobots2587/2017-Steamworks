360b2f8437b9031170481c0dfa874380891a9144
sashankg <sashankg@gmail.com>
2017-02-11 12:11:38 -0600

    Created TriggerToggle class

---
+++ src/org/discobots/steamworks/utils/TriggerToggle.java
@@ -0,0 +1,33 @@
+package org.discobots.steamworks.utils;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.buttons.Trigger;
+
+public class TriggerToggle extends Trigger {
+	
+	private final GenericHID m_joystick;
+	private final int m_axisNumber;	
+	
+	private boolean active;
+	private boolean pressed;
+	
+	public TriggerToggle(GenericHID joystick, int axisNumber) {
+		m_joystick = joystick;
+		m_axisNumber = axisNumber;
+	}
+
+	@Override
+	public boolean get() {
+		// TODO Auto-generated method stub
+		if(pressed) {
+			if(m_joystick.getRawAxis(m_axisNumber) == 0) {
+				active = !active;
+				pressed = false;
+			}
+		}
+		if(m_joystick.getRawAxis(m_axisNumber) == 1) {
+			pressed = true;
+		}
+		return active;
+	}
+}
