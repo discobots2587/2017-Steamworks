c1324f775af4e7a125ece2893b98f19e5137c66b
William Bittner <wbittnet@gmail.com>
2017-02-11 10:47:01 -0600

    shiftware based speed shift

---
+++ src/org/discobots/steamworks/commands/drive/SoftShiftCommand.java
@@ -0,0 +1,51 @@
+package org.discobots.steamworks.commands.drive;
+
+import org.discobots.steamworks.Robot;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class SoftShiftCommand extends Command {
+	/*private int time;
+	private long endTime;
+	private double speed;
+	private boolean fin=false;
+	public boolean end=false;*/
+
+    public SoftShiftCommand() {
+        // Use requires() here to declare subsystem dependencies
+    	requires(Robot.driveTrainSub);
+
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+
+    	Robot.driveTrainSub.setSpeedScaling(1.0);
+    } 
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+
+    	
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.driveTrainSub.setSpeedScaling(0.0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
