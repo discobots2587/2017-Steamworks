0a7bbea9a44fc84bb0f2c105e4cb07a7f77c505d
Mason <mason.s.cole@hotmail.com>
2017-02-16 17:23:16 -0600

    Added Compressor Methods

--- src/org/discobots/steamworks/subsystems/ElectricalSubsystem.java
+++ src/org/discobots/steamworks/subsystems/ElectricalSubsystem.java
@@ -1,5 +1,10 @@
 package org.discobots.steamworks.subsystems;
 
+import org.discobots.steamworks.HW;
+import org.discobots.steamworks.utils.PressureSensor;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -7,13 +12,55 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class ElectricalSubsystem extends Subsystem {
 	public int sensorToggle=0;
+	PowerDistributionPanel pdp;
+	Compressor cmp;
+	PressureSensor ps;
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
+	public ElectricalSubsystem(){
+		pdp = new PowerDistributionPanel();
+		cmp = new Compressor();
+		ps = new PressureSensor(HW.pressureSensor);
+	}
+	
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
-}
+    public double getPDPVoltage() {
+		return pdp.getVoltage();
+	}
 
+	public double getCurrentFromPDPChannel(int channel) {
+		return pdp.getCurrent(channel);
+	}
+
+	public double getPDPTotalCurrent() {
+		double totalCurrent = 0;
+		for (int i = 0; i < 16; i++) {
+			totalCurrent += pdp.getCurrent(i);
+		}
+		return totalCurrent;
+	}
+	public void setCompressor (boolean var){
+		if (var){// could also add (&& getPressure() < MAX PRESSURE VALUE)
+				cmp.start();
+		}else{
+				cmp.stop();
+		}
+	}
+	public boolean getCompressorState(){
+		return cmp.enabled();
+	}
+	public boolean getPressureSwitchState(){
+		return cmp.getPressureSwitchValue();
+	}
+	public double getPressure() {
+		return ps.getPSI();
+	}
+	public double getCompressorControlLoopState(){
+		return cmp.getCompressorCurrent();
+	}
+}
