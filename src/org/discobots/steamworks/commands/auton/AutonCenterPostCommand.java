c4ef5b907519e4b91b6c3ab9a1b863bcf6e19234
Mason <mason.s.cole@hotmail.com>
2017-03-31 18:22:15 -0500

    more angling

--- src/org/discobots/steamworks/commands/auton/AutonCenterPostCommand.java
+++ src/org/discobots/steamworks/commands/auton/AutonCenterPostCommand.java
@@ -32,15 +32,17 @@ public class AutonCenterPostCommand extends CommandGroup {
     	addSequential(new AutonomousArcadeDrive(0,0.7,1500));//straight
     	addSequential(new AutonomousArcadeDrive(0,0.5,1000));
     	addSequential(new WaitCommand(1500));
-    	addSequential(new AutonomousArcadeDrive(-0.2,-0.5, 1000));//backup at angle
-    	addSequential(new AutonomousArcadeDrive(-0.1, 0.55,1200));//forwards again
+    	addSequential(new AutonomousArcadeDrive(-0.2,-0.7, 1000));//backup at angle
+    	addSequential(new AutonomousArcadeDrive(0,-0.7, 400));//backup at angle
+    	addSequential(new AutonomousArcadeDrive(-0.3, 0.65,1500));//forwards again greater angle
     	addParallel(new ExtendHoodCommand(1000));
     	addSequential(new WaitCommand(1500));
-    	addSequential(new AutonomousArcadeDrive(0.2,-0.5, 1000));//backup at angle
-    	addSequential(new AutonomousArcadeDrive(0.1, 0.55,1200));//forwards again
+    	addSequential(new AutonomousArcadeDrive(0.2,-0.7, 1000));//backup at angle
+    	addSequential(new AutonomousArcadeDrive(0,-0.7, 400));//backup no angle
+    	addSequential(new AutonomousArcadeDrive(0.3, 0.65,1500));//forwards again greater angle
     	addSequential(new WaitCommand(1500));
     	addSequential(new AutonomousArcadeDrive(0.2,-0.5, 1000));//backup at angle
-    	addSequential(new AutonomousArcadeDrive(0.1, 0.55,1200));//forwards again
+    	addSequential(new AutonomousArcadeDrive(0.3, 0.55,1200));//forwards again greater angle
     	
     	
     }
