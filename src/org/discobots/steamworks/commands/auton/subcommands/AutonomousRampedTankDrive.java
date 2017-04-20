47e0c750424e092f00f6a7b615a68e3f7c1f3142
Mason <mason.s.cole@hotmail.com>
2017-04-20 11:04:23 -0500

    Working on ramped, scaled auton

--- src/org/discobots/steamworks/commands/auton/subcommands/AutonomousRampedTankDrive.java
+++ src/org/discobots/steamworks/commands/auton/subcommands/AutonomousRampedTankDrive.java
@@ -12,15 +12,24 @@ boolean turnRamp=false;
 boolean straightRamp=false;
 long endTime;
 double speedScale;
+double speedScaleTurn;
 double left;
 double right;
+long startTime;
+double scaledLeft;
+double scaledRight;
+long midTime;
     public AutonomousRampedTankDrive(double left, double right, long timeMili, boolean straightRamp, boolean turnRamp) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	endTime=System.currentTimeMillis()+timeMili;
+    	startTime=System.currentTimeMillis();
     	this.left=left;
     	this.right=right;
     	this.turnRamp=turnRamp;
+    	endTime=System.currentTimeMillis()+(timeMili/2);
+
+    	
     	
     }
 
@@ -31,28 +40,31 @@ double right;
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        		if(!Robot.turnScale&&!Robot.directScale)
+    	 
+        		if(!turnRamp&&!straightRamp)
             	Robot.driveTrainSub.tankDrive(left,-right);
-        		if(Robot.turnScale&&!Robot.directScale){
-        			speedScale = Math.pow(Robot.driveTrainSub.getSpeedScaling(),1-(Math.abs((Robot.oi.getRawAnalogStickALY())+(Robot.oi.getRawAnalogStickARY())/2)));
-                	Robot.driveTrainSub.tankDrive(Robot.oi.getRawAnalogStickARY()*speedScale,-Robot.oi.getRawAnalogStickALY()*speedScale);
+        		if(turnRamp&&!straightRamp){
+        			speedScale = Math.pow(Robot.driveTrainSub.getSpeedScaling(),1-(Math.abs((left)+(right)/2)));
+                	Robot.driveTrainSub.tankDrive(Robot.oi.getRawAnalogStickARY()*speedScale,-left*speedScale);
         		}
-        		if(!Robot.turnScale&&Robot.directScale)
+        		if(!turnRamp&&straightRamp)
         		{
-        			speedScale = Math.pow(Robot.driveTrainSub.getSpeedScaling(),(Math.abs((Robot.oi.getRawAnalogStickALY())+(Robot.oi.getRawAnalogStickARY())/2)));
-                	Robot.driveTrainSub.tankDrive(Robot.oi.getRawAnalogStickARY()*speedScale,-Robot.oi.getRawAnalogStickALY()*speedScale);
+        			speedScale = Math.pow(Robot.driveTrainSub.getSpeedScaling(),(Math.abs((left)+(Robot.oi.getRawAnalogStickARY())/2)));
+                	Robot.driveTrainSub.tankDrive(Robot.oi.getRawAnalogStickARY()*speedScale,-left*speedScale);
         		}
-        		if(Robot.turnScale&&Robot.directScale)
-                	Robot.driveTrainSub.tankDrive(Robot.oi.getRawAnalogStickARY()*Robot.driveTrainSub.getSpeedScaling(),-Robot.oi.getRawAnalogStickALY()*Robot.driveTrainSub.getSpeedScaling());
+        		if(turnRamp&&straightRamp)
+                	Robot.driveTrainSub.tankDrive(Robot.oi.getRawAnalogStickARY()*Robot.driveTrainSub.getSpeedScaling(),-left*Robot.driveTrainSub.getSpeedScaling());
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+    	return System.currentTimeMillis()>=endTime;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.driveTrainSub.tankDrive(0,0);
+
     }
 
     // Called when another command which requires one or more of the same
